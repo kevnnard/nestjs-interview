0e02fec4260ecf8e1d095e786ac6456f257f66ed
KevnnardFedora <londonokevin9@gmail.com>
2024-02-12 15:23:23 -0500

    refactor: update imports and file configs

--- apps/api/src/app/services/auth/auth.module.ts
+++ apps/api/src/app/services/auth/auth.module.ts
@@ -3,14 +3,16 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { PrismaModule } from '@ocmi/api/app/configs/prisma/prisma.module';
+import { ClientModule } from '@ocmi/api/app/services/client/client.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtAdminStrategy } from './strategies/admin.strategy';
+import { JwtClientStrategy } from './strategies/client.strategy';
 
 @Module({
   imports: [
     ConfigModule,
-    PassportModule.register({ defaultStrategy: 'jwt' }),
+    PassportModule.register({ session: true }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -20,9 +22,10 @@ import { JwtAdminStrategy } from './strategies/admin.strategy';
       }),
     }),
     PrismaModule,
+    ClientModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtAdminStrategy],
-  exports: [JwtAdminStrategy, PassportModule, JwtModule],
+  providers: [AuthService, JwtAdminStrategy, JwtClientStrategy],
+  exports: [JwtAdminStrategy, JwtClientStrategy, PassportModule, JwtModule],
 })
 export class AuthModule {}
