fd9beceb6d21a598f18d870db85617490d851c49
KevnnardFedora <londonokevin9@gmail.com>
2024-02-12 16:55:40 -0500

    feat: add constant to responses in methods

---
+++ apps/api/src/app/services/employee/constants/responses.ts
@@ -0,0 +1,5 @@
+// Object that contains the responses for the client service methods
+export const RESPONSES = {
+  EMPLOYEE_NOT_FOUND: 'Employee not found, please select another ID',
+  EMPLOYEE_ALREADY_EXISTS: 'Employee already exist, please select another name',
+};
