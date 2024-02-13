3753175b3f1f9259904d9948edc1f4a1fba37f71
KevnnardFedora <londonokevin9@gmail.com>
2024-02-13 18:23:28 -0500

    refactor: update imports to test

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { ConfigModule } from '@nestjs/config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { PrismaModule } from '@ocmi/api/app/configs/prisma/prisma.module';
 import { AuthModule } from '@ocmi/api/app/services/auth/auth.module';
 import { ClientModule } from '@ocmi/api/app/services/client/client.module';
 import { EmployeeModule } from '@ocmi/api/app/services/employee/employee.module';
 import { HelloCommand } from '@ocmi/api/commands/hello.command';
-import { PrismaModule } from 'nestjs-prisma';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './services/admin/admin.module';
