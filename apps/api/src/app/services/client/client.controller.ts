b8b4105a5bc1aced395c7737862413d08f94399b
KevnnardFedora <londonokevin9@gmail.com>
2024-02-12 23:36:59 -0500

    feat: add Endpoints to client

--- apps/api/src/app/services/client/client.controller.ts
+++ apps/api/src/app/services/client/client.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Client, TimeSheet } from '@prisma/client';
+import { CurrentClient } from '../auth/decorators/client.decorator';
 import { JwtClientGuard } from '../auth/guards/client.guard';
 import { ClientService } from './client.service';
 
@@ -6,4 +8,19 @@ import { ClientService } from './client.service';
 @Controller('client')
 export class ClientController {
   constructor(private readonly clientService: ClientService) {}
+
+  // This method is used to get the timesheets for the client. It returns the timesheets.
+  @Get('timesheets')
+  async getTimesheets(@CurrentClient() client: Client): Promise<TimeSheet[]> {
+    return await this.clientService.getTimesheets(client);
+  }
+
+  // This method is used to create a timesheet for the client. It returns the timesheet.
+  @Post('timesheet/create')
+  async createTimesheet(
+    @Body() data: [number],
+    @CurrentClient() client: Client,
+  ) {
+    return await this.clientService.createTimesheet(data, client);
+  }
 }
